7f697be557772b3dd64ce430571785fd7354bce2
Joshua Mestre <joshua.mestre@outlook.com>
2018-05-30 10:11:50 +0200

    Initial Commit

---
+++ TravelBag/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TravelBag
+//
+//  Created by Anonymer Eintrag on 30.05.18.
+//  Copyright © 2018 Gruppe04. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
