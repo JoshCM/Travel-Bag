5d2d268650132dfd17e4d9a6c61d8da2a917580e
Joshua Mestre <joshua.mestre@outlook.com>
2018-05-30 13:03:24 +0200

    fast fertig...

--- TravelBag/ViewController.swift
+++ TravelBag/ViewController.swift
@@ -7,12 +7,53 @@
 //
 
 import UIKit
+import MapKit
+
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var searchField: UITextField!
+    let initialZoom:CLLocationDistance! = 1000000
+    var regionRadius :CLLocationDistance?
+    let initLoc = CLLocation(latitude: 50.095845, longitude:8.218644)
+    var actLoc : CLLocation!
+    
+    func centerMaponLocation(location:CLLocation){
+        let coordRegion = MKCoordinateRegionMakeWithDistance(location.coordinate, regionRadius!, regionRadius!)
+        mapView.setRegion(coordRegion, animated: true)
+    }
+    
+    @IBAction func searchButton(_ sender: Any) {
+        let goec = CLGeocoder()
+        goec.geocodeAddressString(searchField.text!, completionHandler: {(placemarks,error)->Void in
+            
+            var location:CLLocation?
+            
+            if let placemarks = placemarks, placemarks.count > 0 {
+                location = placemarks.first?.location
+            }
+            
+            if let location = location {
+                self.actLoc = location
+                self.regionRadius! /= 10
+                self.centerMaponLocation(location: location)
+                self.regionRadius! *= 10
+            } else {
+                print("No Matching Location Found")
+            }
+        })
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        mapView.isRotateEnabled = false
+        searchField.delegate = self
+        actLoc = initLoc
+        self.regionRadius = initialZoom
+        let marker = Marker(title: "Hochschule", locationName: "unter den Eichen", discipline: "Education", coordinate: self.initLoc.coordinate)
+        centerMaponLocation(location: initLoc)
+        mapView.addAnnotation(marker)
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -22,4 +63,32 @@ class ViewController: UIViewController {
 
 
 }
+extension ViewController:UITextFieldDelegate{
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        self.searchButton(self)
+        return true
+    }
+    
+    func textFieldShouldClear(_ textField: UITextField) -> Bool {
+        regionRadius = initialZoom
+        centerMaponLocation(location: actLoc)
+        
+        return true
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
